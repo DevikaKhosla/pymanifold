55f7e834a1669fc34e2601d75ab44f343057e303
DevikaKhosla <dkhosla_9@hotmail.com>
2018-11-28 23:24:08 -0500

    Add analyte properties (diffusivity, conc, radii, charges)
    Instead of adding it directly to the properties dictionary, I created
    another dictionary for the analyte properties.
    
    Also added get functions for each property

--- src/constants.py
+++ src/constants.py
@@ -1,10 +1,20 @@
 # May want to make it a text file so its easier
 class FluidProperties():
 
-    PROPERTIES = {"default": [False, False, False],
-                  "water": [999.87, 18200, 0.001],
-                  "mineraloil": [800, 10000000000, 0.0003051],
-                  "polyacrylamide": [1100, 14.28, 0.003]}
+    PROPERTIES = {"default": [False, False, False, "none"],
+                  "water": [999.87, 18200, 0.001, "none"],
+                  "mineraloil": [800, 10000000000, 0.0003051, "none"],
+                  "polyacrylamide": [1100, 14.28, 0.003, "none"],
+                  "ep_cross_test_sample": [999.87, 18200, 0.001, "ep_cross_test_analyte"]}
+
+    # properties specific to samples for ep_cross (contain multiple analytes, which can vary in properties)
+    ANALYTE_PROPERTIES = {"default": [False, False, False, False],
+                          "none": [False, False, False, False],
+                          "ep_cross_test_analyte": [ [0.1, 0.1, 0.1, 0.1],
+                                                     [0.2, 0.2, 0.2, 0.2],
+                                                     [0.05, 0.05, 0.05, 0.05],
+                                                     [-1, -2, -3, -4]] }
+                                                 # ep_cross_test_analyte numbers are made up
 
     def getDensity(self, fluid_name):
             return self.PROPERTIES[fluid_name][0]
@@ -14,3 +24,20 @@ class FluidProperties():
 
     def getViscosity(self, fluid_name):
             return self.PROPERTIES[fluid_name][2]
+
+
+    def getDiffusivities(self, fluid_name):
+        analyte_name = self.PROPERTIES[fluid_name][3]
+        return self.ANALYTE_PROPERTIES[analyte_name][0]
+
+    def getInitialConcentrations(self, fluid_name):
+        analyte_name = self.PROPERTIES[fluid_name][3]
+        return self.ANALYTE_PROPERTIES[analyte_name][1]
+
+    def getRadii(self, fluid_name):
+        analyte_name = self.PROPERTIES[fluid_name][3]
+        return self.ANALYTE_PROPERTIES[analyte_name][2]
+
+    def getCharges(self, fluid_name):
+        analyte_name = self.PROPERTIES[fluid_name][3]
+        return self.ANALYTE_PROPERTIES[analyte_name][3]
