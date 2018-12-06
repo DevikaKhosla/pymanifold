36cbf927536f3cb226f72b4b0b733ca4b96a9b35
DevikaKhosla <dkhosla_9@hotmail.com>
2018-12-06 15:57:18 -0500

    fix ep_cross test
    change cathode to an input

--- tests/ep_cross_test.py
+++ tests/ep_cross_test.py
@@ -19,7 +19,7 @@ cathode_voltage = 0
 anode_voltage = 2
 
 # syntax: sch.elec_port(name, design[, voltage, pressure, flow_rate, density, X_pos, Y_pos])
-sch.elec_port(cathode_node, 'output', voltage=cathode_voltage, min_pressure=1) #, fluid_name='mineraloil')
+sch.elec_port(cathode_node, 'input', voltage=cathode_voltage, min_pressure=1) #, fluid_name='mineraloil')
 sch.elec_port(anode_node, 'output', voltage=anode_voltage)
 # normal ports do not have voltages; syntax is otherwise the same
 sch.port(input_node, 'input', min_pressure=1, fluid_name='ep_cross_test_sample')
@@ -29,7 +29,8 @@ sch.port(waste_node, 'output')
 sch.node(junction_node, 1, 1, kind='ep_cross')
 
 # syntax: sch.channel(shape, min_length, width, height, input, output)
-sch.channel(cathode_node, junction_node)
+sch.channel(cathode_node, junction_node,
+			phase = 'tail')
 sch.channel(junction_node, anode_node,
 			phase = 'separation')
 sch.channel(input_node, junction_node)
